68bbcb3ce173a4d3c17058d9d3072cc904cfe632
João Pedro <135253608+joaopedrojayp@users.noreply.github.com>
2023-07-11 15:49:26 -0300

    atualizei

---
+++ projetos/x-men/js/index.js
@@ -0,0 +1,14 @@
+   //* ===== SCRIPT! ======*//
+
+const personagens = document.querySelectorAll(".personagem");
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        const personagemSelecionado = document.querySelector
+        ('.selecionado');
+        personagemSelecionado.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado'); 
+    })
+})
+
