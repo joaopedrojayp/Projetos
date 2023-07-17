e2f0ffac8b06f76d9e1223acca45aa993748ff8e
João Pedro <135253608+joaopedrojayp@users.noreply.github.com>
2023-07-17 11:32:18 -0300

    tirei

--- projetos/Loja 2/index.js
+++
@@ -1,8 +0,0 @@
-// == VARIAVEIS!
-let menu = document.querySelector('#menu-bar')
-let navbar = document.querySelector('.navbar')
-
-menu.onclick =() =>{
-    menu.classList.toggle('bx-menu');
-    navbar.classList.toggle('active');
-}
